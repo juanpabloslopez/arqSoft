7442aaae0dd6443dd29ab8d9ebdab15f90bf06b0
juanpabloslopez <jpschiaffino@gmail.com>
2024-06-05 20:30:49 -0400

    archivos profe ahora si funcionando

--- antiprofe.py
+++ antiprofe.py
@@ -1,41 +1,41 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+# Crear un socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the bus is listening
+# Conectar el socket al puerto donde el bus está escuchando
 bus_address = ('localhost', 5000)
-print ('connecting to {} port {}'.format (*bus_address))
-sock.connect (bus_address)
+print('connecting to {} port {}'.format(*bus_address))
+sock.connect(bus_address)
 
 try:
-    # Send data
+    # Enviar datos
     message = b'00010sinitservi'
-    print ('sending {!r}'.format (message))
-    sock.sendall (message)
+    print('sending {!r}'.format(message))
+    sock.sendall(message)
     sinit = 1
+
     while True:
-        # Look for the response
-        print ("Waiting for transaction")
+        # Esperar la transacción
+        print("Waiting for transaction")
         amount_received = 0
-        amount_expected = int(sock.recv (5))
+        amount_expected = int(sock.recv(5))
 
-    while amount_received < amount_expected:
-        data = sock.recv (amount_expected - amount_received)
-        amount_received += len (data)
-    print ("Procesing ...")
-    print('received {!r}'.format(data))
+        while amount_received < amount_expected:
+            data = sock.recv(amount_expected - amount_received)
+            amount_received += len(data)
+        print("Procesing ...")
+        print('received {!r}'.format(data))
+        if sinit == 1:
+            sinit = 0
+            print('Received sinit answer')
+        else:
+            print("Send answer")
+            message = b'00013serviReceived'
+            print('sending {!r}'.format(message))
+            sock.sendall(message)
 
-    if (sinit == 1):
-        sinit = 0
-        print ('Received sinit answer')
-    else:
-        print ("Send answer")
-        message = b'00013serviReceived'
-        print ('sending {!r}'.format (message))
-        sock.sendall (message)
 finally:
-    print ('closing socket')
-    sock.close ()
-
+    print('closing socket')
+    sock.close()
--- profe.py
+++ profe.py
@@ -1,33 +1,34 @@
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+# Crear un socket TCP/IP
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the bus is listening
+# Conectar el socket al puerto donde el bus está escuchando
 bus_address = ('localhost', 5000)
-print ('connecting to {} port {}'.format (*bus_address))
-sock.connect (bus_address)
+print('connecting to {} port {}'.format(*bus_address))
+sock.connect(bus_address)
 
 try:
     while True:
-        # Send Hello world to servi
-        if (input ('Send Hello world to servi ? y/n: ') != 'y'):
+        # Enviar "Hello world" al servicio
+        if input('Send Hello world to servi? y/n: ') != 'y':
             break
         message = b'00016serviHello world'
-        print ('sending {!r}'.format (message))
-        sock.sendall (message)
+        print('sending {!r}'.format(message))
+        sock.sendall(message)
 
-        # Look for the response
-        print ("Waiting for transaction")
+        # Esperar la respuesta
+        print("Waiting for transaction")
         amount_received = 0
-        amount_expected = int(sock.recv (5))
+        amount_expected = int(sock.recv(5))
 
         while amount_received < amount_expected:
-            data = sock.recv (amount_expected - amount_received)
-            amount_received += len (data)
-        print ("Checking servi answer ...")
+            data = sock.recv(amount_expected - amount_received)
+            amount_received += len(data)
+        print("Checking servi answer ...")
         print('received {!r}'.format(data))
+
 finally:
-    print ('closing socket')
-    sock.close ()
+    print('closing socket')
+    sock.close()
